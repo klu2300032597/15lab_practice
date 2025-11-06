61edd1fb754533b5c9396262984cfe71c46546f9
klu2300032597 <2300032597@kluniversity.in>
2025-11-06 19:56:22 +0530

    3.0

---
+++ backend/product/src/main/java/com/klef/cicd/ProductApplication.java
@@ -0,0 +1,15 @@
+package com.klef.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class ProductApplication extends SpringBootServletInitializer{
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductApplication.class, args);
+		System.out.println("project is running");
+	}
+
+}
